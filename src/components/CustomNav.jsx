8a752c408671d1a75854b7aa088a880509844abb
Sreeplays <sriram.7b17@gmail.com>
2024-04-17 13:07:06 +0530

    Added menu function and tweaked the home page

--- src/components/CustomNav.jsx
+++ src/components/CustomNav.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react'
 import logo from '../assets/Sreelogo-1.jpg'
 import { Navbar } from 'react-bootstrap'
-import { NavbarBrand } from 'reactstrap'
+import { NavLink, NavbarBrand } from 'reactstrap'
 import Blog from '../assets/BlogApp.png'
-import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineClose, AiOutlineHome} from 'react-icons/ai'
 const CustomNav = () => {
   const [nav, setNav] = useState(false)
-  const handleClick = setNav(!nav)
+  const handleClick = () => setNav(!nav)
   return (
     <div>
       <Navbar className='px-2 flex w-full h-full justify-between backdrop-blur-0'>
@@ -22,10 +22,18 @@ const CustomNav = () => {
             </div>
           </div>
         </div>
-        <div className='md:hidden absolute right-1 top-6' onClick={handleClick}>
-        {!nav ? (<AiOutlineMenu />) : (<AiOutlineClose className=' relative '/>)}
+        <div className='md:hidden absolute top-4 right-1' onClick={handleClick}>
+          {!nav ? (<AiOutlineMenu />) : (<AiOutlineClose className=' relative '/>)}
+            
         </div>
+
       </Navbar>
+      <div className={!nav ? 'hidden ease-in-out delay-500 relative border-none' : 'w-full ease-in-out pt-2 pr-2 delay-500 bg-opacity-40'}>
+        <div className='pl-5 border-b border-zinc-500'><NavLink href='/' className='flex'>Home <AiOutlineHome className='w-6 mt-1' /></NavLink></div>
+        <div className='pl-5 border-b border-zinc-500'>Blog</div>
+        <div className='pl-5 border-b border-zinc-500'>About</div>
+        <div className='pl-5 border-b border-zinc-500'>Contact</div>
+      </div>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <div name='home' className='w-full h-screen flex flex-col justify-between'>
-        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto'>
+        <div className='grid md:grid-cols-2 max-w-[1240px] m-auto pl-10'>
             <div className='flex flex-col justify-between md:justify-center w-full pl-10 pr-10 py-8'>
                 <h1 className='py-3 text-xl md:text-3xl font-bold'>Sreeplays</h1>
                 <p className='md:text-lg text-sm'>Welcome to Sreeplays! You can write out all your heart here and read as much as the blogs are there.</p>
