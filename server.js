6a67cf29f463a7f020947ce639043e9b53d7595e
Sreeplays <sriram.7b17@gmail.com>
2025-04-20 13:42:43 +0530

    created the uploads link so that frontend can fetch images

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path"
 import connectedDB from "./config/db.js";
 import routes from "./routes/userRoutes.js";
 import { errorResponseHandler, invalidURLHandler } from "./middleware/errorHandler.js";
+import { fileURLToPath } from "url";
 
 dotenv.config();
 connectedDB()
@@ -14,6 +16,11 @@ app.get('/', (req, res) => {
     res.send("Server is running!!")
 })
 app.use('/api/users', routes)
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
 app.use(invalidURLHandler)
 app.use(errorResponseHandler)
 
